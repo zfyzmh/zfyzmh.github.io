2588cd957d5efa6d51942a57e1e3d8bf33b0a6d3
子非鱼 <96179195+zfyzmh@users.noreply.github.com>
2024-02-02 11:50:11 +0800

    修改配置文件

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -47,14 +47,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/zfyzmh/zfyzmh.github.io',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          // editUrl:
+          //   'https://github.com/zfyzmh/zfyzmh.github.io',
         },
         theme: {
           customCss: './src/css/custom.css',
